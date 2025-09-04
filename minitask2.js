99908967652c33933b412aefae36d0d963ae1d3e
Muhammad Hilmy Haidar A <85333182+hilmy07@users.noreply.github.com>
2025-09-04 11:27:32 +0700

    Add files via upload

---
+++ minitask2.js
@@ -0,0 +1,25 @@
+const bio = {
+  name: "Muhammad Hilmy",
+  age: 27,
+};
+
+const extraInfo = {
+  skills: ["sport", "painting"],
+  education: [
+    {
+      name: "SMP 1 Denpasar",
+      year: 2016,
+    },
+  ],
+};
+
+const profile = {
+  ...bio,
+  ...extraInfo,
+};
+
+let { skills } = extraInfo;
+console.log(skills[1]);
+
+let { name } = extraInfo.education[0];
+console.log(name);
